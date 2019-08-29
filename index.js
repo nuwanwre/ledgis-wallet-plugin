f48bab3c37baa091682cfac9b65b429c032f1ab0
Nuwan W <nuwanwre@live.com>
2019-08-29 16:27:30 +0900

    impl: added default functional class

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
-export default function helloWorld() {
-    return "Hello World!";
+export default class ecrx {
+    constructor(){
+        
+    }
+
+    helloWorld() {
+        return "Hello World!";
+    }
 }
