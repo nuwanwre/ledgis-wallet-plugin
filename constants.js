d17e9ba92da8a8d5989a451d88231e7e362d5393
Nuwan W <nuwanwre@live.com>
2019-08-30 10:44:33 +0900

    impl: added request functions

--- constants.js
+++ constants.js
@@ -3,3 +3,11 @@ export class UserAgent {
     USER_AGENT_ANDROID = 'android';
     USER_AGENT_WEB = 'web';
 }
+
+export class WalletConstants {
+    WALLET_NAME = 'sigprovider';
+}
+
+export class Actions {
+    WALLET_LOGIN = 'login';
+}
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import uuid from 'uuid';
 
-import { UserAgent } from './constants';
+import { Actions, WalletConstants } from './constants';
 
 /**
  * @classdesc Represents the ECRX Wallet SDK. It allows the client applications to integrate with wallet functionalities
@@ -15,21 +15,28 @@ export default class ecrx {
     constructor(options) {
         this.callbackURL = options.callbackURL;
         this.clientId = uuid.v4();
+        this.connectWebSocket();
+    }
+    
+    /**
+     * Connects to websocket and binds with associated events
+     */
+    connectWebSocket() {
         this.webSocket = new WebSocket(`${this.callbackURL}/?id=${this.clientId}`);
-
+    
         // Attach event listeners
         this.webSocket.onopen = () => {
             this.connected = true;
         };
-
+    
         this.webSocket.onclose = () => {
             this.connected = false;
         }
-
+    
         this.webSocket.onerror = (e) => {
             console.error("ERR ecrx: ", e);
         }
-
+    
         this.webSocket.onmessage = (e) => {
             handleResponse(e);
         }
@@ -41,21 +48,32 @@ export default class ecrx {
      * a JSON object bearing the account information or an Error detailing the issue
      */
     getAccount() {
+        const request = {
+            action: Actions.WALLET_LOGIN
+        }
 
+        return new Promise((resolve, reject) => {
+            
+        })
     }
 
     /**
      * Handle incoming responses from the websocket
-     * @param {String} - Contains the response relayed throught the callback server
+     * @param {String} response - Contains the response relayed throught the callback server
      * @return {Promise} Account - Returns a promise that, when fulfilled, will either return 
      * a JSON object bearing the account information or an Error detailing the issue
      */
-    getAccount() {
-
+    handleResponse(response) {
+        throw "Not implemented exception";
     }
 
-    helloWorld() {
-        return "Hello World!";
+    /**
+     * Invoke ECRX Wallet
+     * @param {String} request - JSON object containing the request that needs to be fulfilled
+     */
+    invokeWallet(request) {
+        const location = generateDeepLink(request);        
+        window.location(location);
     }
 }
 
@@ -70,6 +88,8 @@ class Utils {
      * @param {String} request.body - A JSON object containing the actionab
      */
     static generateDeepLink(request) {
-        throw "Not implemented";
+        if (request.action === Actions.WALLET_LOGIN) {
+            return `${WalletConstants.WALLET_NAME}://request?payload=${request.WALLET_LOGIN}`;
+        }
     }
 }
